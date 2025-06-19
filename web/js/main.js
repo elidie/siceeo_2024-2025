b52631caf22ecf8dbd95da89e0a9e762ad8e6ec6
Eli <Eli@DESKTOP-ELI-DIE>
2025-06-19 09:56:29 -0600

    - Correccion de apartado datos del director, con base de datos.

--- web/js/main.js
+++ web/js/main.js
@@ -102,7 +102,7 @@ function initInformacion ()
     $('#lblUnidad').text(sisVars.unidad);
     $('#lblIp').text(sisVars.ip);
     $('#lblFecha').text(sisVars.fecha);
-    if ( $('#lblVersionSis').html().trim().indexOf(sisVars.versionSis) === -1  || sisVars.versionSis!=="15.7.1") {
+    if ( $('#lblVersionSis').html().trim().indexOf(sisVars.versionSis) === -1  || sisVars.versionSis!=="15.7.2") {
         $("#lblVersionSis").css("color","red");
         mensaje.Principal("VERSION_NO_ACTUALIZADA",sisVars.versionSis);
     } else {
